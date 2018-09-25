66379db29ff28e5a6f7b6c2b3d34c18a093096ec
DESKTOP-KIU0CU0\User <diegocolliprofessional@gmail.com>
2018-09-25 11:21:25 -0300

    Before that I have executed the add-migrations initialcreate and update data-base Now I just have added a regular controller and two regular pages

---
+++ DDDExampleII.Presentation.MVC/Controllers/FundTransferController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DDDExampleII.Presentation.MVC.Controllers
+{
+    public class FundTransferController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "This is the example Page.";
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            ViewData["Message"] = "This is the List Page.";
+            return View();
+        }
+
+    }
+}
